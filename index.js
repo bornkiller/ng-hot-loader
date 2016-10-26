a1910b2fba62493ef774cd862a89cc3d0c2528b1
黄健 <jian.huang03@ele.me>
2016-10-26 18:38:51 +0800

    feature: factory HMR.

--- index.js
+++ index.js
@@ -5,9 +5,34 @@
 'use strict';
 
 import path from 'path';
+import util from 'loader-utils';
+import { camelCase, capitalize } from 'lodash';
 
-module.exports = function (source) {
+module.exports = function (input) {
   this.cacheable && this.cacheable();
 
-  return source;
+  const query = util.parseQuery(this.query);
+
+  const prefix = query.prefix;
+  const basename = path.basename(this.resourcePath, '.factory.js');
+  const targetName = !!prefix ? prefix + capitalize(camelCase(basename)) : camelCase(basename);
+  const targetFactory = camelCase(basename) + 'Factory';
+
+  return `${input}
+    if (module.hot) {
+      module.hot.accept();
+  
+      var element = angular.element(document.body);
+      var $injector = element.injector();
+  
+      if ($injector) {
+        var prevTargetService = $injector.get('${targetName}');
+        var hotTargetService = $injector.invoke(${targetFactory});
+        var $rootScope = $injector.get('$rootScope');
+        
+        angular.extend(prevTargetService, hotTargetService);
+        $rootScope.$apply();
+      }
+    }
+  `;
 };
